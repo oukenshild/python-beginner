a6b7917a4dc3ea8c711cee20351c6166434d995d
Danil <danililukhin@yandex.ru>
2023-08-21 22:27:30 +0500

    Добавить ДЗ geo_id

---
+++ 4. collections/geo_id/geo_id.py
@@ -0,0 +1,8 @@
+ids = {'user1': [213, 213, 213, 15, 213],
+       'user2': [54, 54, 119, 119, 119],
+       'user3': [213, 98, 98, 35]}
+
+result = set()
+for i in ids.values():
+    result.update(i)
+print(result)
